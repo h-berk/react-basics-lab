bfdd66963b78ee8f8b3e22c8d7aeef71e1c45a18
Hasan Berk <hasanberk50@gmail.com>
2022-10-05 20:35:42 +0100

    Exercise 4 completed.

--- src/stories/exercises/04-components.stories.js
+++ src/stories/exercises/04-components.stories.js
@@ -7,27 +7,31 @@ export default {
 };
 
 export const Basic = () => {
-  const name = "HDip Computer Science";
+  const name = "BSc Honours in Applied Computing Cloud and Networks";
   const modules = [
-    {
-      name: "DevOps",
-      noLectures: 1,
-      noPracticals: 2,
-    },
-    {
-      name: "Enterprise Web Dev",
-      noLectures: 2,
-      noPracticals: 3,
-    },
-    {
-      name: "Programming",
-      noLectures: 2,
-      noPracticals: 2,
-    },
-    {
-      name: "Computer Systems",
-      noLectures: 2,
-      noPracticals: 3,
+    {name: "Dev Ops",
+     noLectures: 1,
+     noPracticals: 3
+    }, 
+    {name: "Web App Dev",
+    noLectures: 2,
+    noPracticals: 2
+    }, 
+    {name: "Wireless Communications",
+    noLectures: 2,
+    noPracticals: 2
+    }, 
+    {name: "NOSQL Databases",
+    noLectures: 2,
+    noPracticals: 2
+    }, 
+    {name: "Network Forensics",
+    noLectures: 1,
+    noPracticals: 3
+    }, 
+    {name: "Professional Practice",
+    noLectures: 2,
+    noPracticals: 2
     },
   ];
   return <CourseModulesCollections modules={modules} course={name} />;
