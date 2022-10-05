6cee0979845678d652dbbb6048d0857b68ee27d6
Hasan Berk <hasanberk50@gmail.com>
2022-10-05 16:32:05 +0100

    Exercise 1 complete

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -3,7 +3,50 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>BSc Honours in Applied Computing Cloud and Networks</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody >
+          <tr>
+            <td>DevOps</td>
+            <td>1</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>Web App Dev</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>Wireless Communications</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>NOSQL Databases</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>Network Forensics</td>
+            <td>1</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>Professional Practice</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+        </tbody >
+      </table>
+    </div>
   );
 };
 
